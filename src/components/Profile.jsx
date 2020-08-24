cd21cd47e9a5a95f83e3b5a321776e3e7912b6d5
Elnar Gadzhiev <gadzhiev_elnar@mail.ru>
2020-08-24 22:55:32 +0300

    comment

---
+++ src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://secrets-fitness.ru/images/u/pages/gym-cover-10.jpg"></img>
+            </div>
+
+            <div>
+                ava + description
+            </div>
+
+            <div>
+                My posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    posts
+                    <div>
+                            post1
+                    </div>
+                    <div>
+                            post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
