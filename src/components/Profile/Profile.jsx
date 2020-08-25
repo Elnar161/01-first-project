c605437aa8f42b9b5ea4ee62d903dd601e77f0fd
pavel_m <pavel_m@NET>
2020-08-25 13:08:53 +0300

    19

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,7 +6,7 @@ const Profile = () => {
     return (
         <div>
             <div>
-                <img src="https://secrets-fitness.ru/images/u/pages/gym-cover-10.jpg"></img>
+                <img src="https://avatars.mds.yandex.net/get-pdb/477388/8ea85076-5274-4d1e-b4b8-5577191e5d25/s1200?webp=false"></img>
             </div>
 
             <div>
