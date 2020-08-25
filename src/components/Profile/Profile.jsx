90f76bdfaa016110df3a8c8708e0be99933dddc2
pavel_m <pavel_m@NET>
2020-08-25 12:15:56 +0300

    18

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src="https://secrets-fitness.ru/images/u/pages/gym-cover-10.jpg"></img>
             </div>
