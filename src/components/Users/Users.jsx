6bdc4c292b9f88014db4a37cd15215d6da880f21
Elnar Gadzhiev <gadzhiev_elnar@mail.ru>
2020-10-15 23:17:58 +0300

    56 Presentation and Container Components

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,56 +1,19 @@
-import Axios from 'axios';
 import React from 'react';
 import s from './Users.module.css';
 import userPhoto from '../../assets/images/user.png'
 
-class Users extends React.Component{
-    
-    //если конструктор просто прокидывает пропсы в конструктор родителя
-    //тогда его можно опустить. это произойдет автоматом
-    constructor(props){
-        super(props);        
-    }
-
-    getUsers = () => {
-        if (this.props.users.length === 0) {
-            debugger;
-            //setTimeout(
-            //    () => {
-            Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-                //.then(response => {console.log(response.data.items);})
-                .then(response => { this.props.setUsers(response.data.items, response.data.totalCount) })
-            //}, 5000)
-        }
-    }
-
-    componentDidMount(){
-        //вызывается сразу после монтирования (то есть, вставки 
-        //компонента в DOM). В этом методе должны происходить действия, 
-        //которые требуют наличия DOM-узлов. 
-        //Это хорошее место для создания сетевых запросов.
-        this.getUsers();
-    }
-
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber);
-        Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-        //.then(response => {console.log(response.data.items);})
-        .then(response => { this.props.setUsers(response.data.items, response.data.totalCount) })        
-    }
-
-    //метод render обязателен
-    render(){
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+let Users = (props) => {
+        let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
         
         let arrPages = [];
         
         for (let i = 1; i <= pagesCount; i++) {
-            let el = <span className={this.props.currentPage === i && s.selectedPage} 
-                           onClick={(e) => { this.onPageChanged(i); }}>{i}</span>;
+            let el = <span className={props.currentPage === i && s.selectedPage} 
+                           onClick={(e) => { props.onPageChanged(i); }}>{i}</span>;
             arrPages.push(el);
         }
 
-        let arrUsers = this.props.users.map((u) => 
+        let arrUsers = props.users.map((u) => 
         <div key={u.id}>
             <span>
                 <div>
@@ -59,9 +22,9 @@ class Users extends React.Component{
                 <div>
                     {
                         u.followed ? 
-                        <button onClick={() => this.props.unfollow(u.id)}>Unfollow</button>                     
+                        <button onClick={() => props.unfollow(u.id)}>Unfollow</button>                     
                         :
-                        <button onClick={() => this.props.follow(u.id)}>Follow</button>                     
+                        <button onClick={() => props.follow(u.id)}>Follow</button>                     
                     }
                     
                 </div>
@@ -86,9 +49,7 @@ class Users extends React.Component{
             </span>
         </div>);
     
-        return (       
-            
-            
+        return (                           
             <div>
                 <div>
                     {arrPages}
@@ -98,7 +59,6 @@ class Users extends React.Component{
                 </div>
             </div>
         );
-    }
 }
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,60 @@
+import Axios from 'axios';
 import React from 'react';
 import { connect } from 'react-redux';
 import { followActionCreator, setUsersActionCreator, unfollowActionCreator, setCurrentPageActionCreator } from '../../redux/UsersReducer';
 import Users from './Users';
+
+
+class UsersApiComponent extends React.Component{
+    
+    //если конструктор просто прокидывает пропсы в конструктор родителя
+    //тогда его можно опустить. это произойдет автоматом
+    constructor(props){
+        super(props);        
+    }
+
+    getUsers = () => {
+        if (this.props.users.length === 0) {
+            debugger;
+            //setTimeout(
+            //    () => {
+            Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+                //.then(response => {console.log(response.data.items);})
+                .then(response => { this.props.setUsers(response.data.items, response.data.totalCount) })
+            //}, 5000)
+        }
+    }
+
+    componentDidMount(){
+        //вызывается сразу после монтирования (то есть, вставки 
+        //компонента в DOM). В этом методе должны происходить действия, 
+        //которые требуют наличия DOM-узлов. 
+        //Это хорошее место для создания сетевых запросов.
+        this.getUsers();
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+        //.then(response => {console.log(response.data.items);})
+        .then(response => { this.props.setUsers(response.data.items, response.data.totalCount) })        
+    }
+
+    //метод render обязателен
+    render(){
+
+        return (       
+            <Users totalUsersCount={this.props.totalUsersCount}
+                   pageSize={this.props.pageSize}
+                   currentPage={this.props.currentPage}
+                   users={this.props.users}
+                   unfollow={this.props.unfollow}
+                   follow={this.props.follow} 
+                   onPageChanged={this.onPageChanged}/>
+        );
+    }
+}
+
 
 let mapStateToProps = (state) => {
     return{
@@ -29,4 +82,4 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersApiComponent);
