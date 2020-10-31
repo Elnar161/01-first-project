0463b9aee4cd4ed621d0dd27c802bede1a5f1dea
Elnar Gadzhiev <gadzhiev_elnar@mail.ru>
2020-10-31 16:10:30 +0300

    93 paginator

--- src/components/Common/Paginator/Paginator.jsx
+++ src/components/Common/Paginator/Paginator.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
+import { useState } from 'react';
 import s from './Paginator.module.css';
 
-let Paginator = ({totalCount, pageSize, currentPage, onPageChanged}) => {
-        let pagesCount = Math.ceil(totalCount / pageSize);
+let Paginator = ({totalCount, pageSize, currentPage, onPageChanged, portionSize = 10}) => {
+       
+    debugger;
+    let pagesCount = Math.ceil(totalCount / pageSize);
         
-        let arrPages = [];
+        let pages = [];
         
         for (let i = 1; i <= pagesCount; i++) {
-            let el = <span className={currentPage === i && s.selectedPage} 
-                           onClick={(e) => { onPageChanged(i); }}>{i}</span>;
-            arrPages.push(el);
+            pages.push(i);
         }
+
+        let portionCount = Math.ceil(pagesCount/portionSize);
+        let [portionNumber, setPortionNumber] = useState(1);
+        let lefrPositionPageNumber = (portionNumber - 1) * portionSize + 1;
+        let  rightPortionPageNumber = portionNumber * portionSize;
+
 
         return (                           
             <div>
-                {arrPages}
+                { portionNumber > 1 && 
+                  <button onClick={() => {setPortionNumber(portionNumber - 1)}}>prev</button>}
+
+                {
+                    pages.filter((p) => (p>=lefrPositionPageNumber) && (p<=rightPortionPageNumber))
+                         .map((p) => {return <span className={currentPage === p && s.selectedPage} 
+                         onClick={(e) => { onPageChanged(p); }}>{p}</span>})
+                }
+
+
+                { portionCount > portionNumber &&
+                  <button onClick={() => {setPortionNumber(portionNumber + 1)}}>next</button> }
             </div>
         );
 }
